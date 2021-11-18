d22b1cf08340744c29aa75f44707eb537387e06c
Rakhi Jaiswal <rakhijaiswal472@gmail.com>
2021-11-18 09:57:06 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text> Welcome </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
