33e5d34f51bafa904ee42454ca251f13f1767d0e
Rakhi Jaiswal <rakhijaiswal472@gmail.com>
2021-11-18 15:52:11 +0530

    Firebase Android

--- src/Screens/AuthStack/Screens/LoginScreen.tsx
+++ src/Screens/AuthStack/Screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {LoginUserAction} from '../../../store/Actions/UserAction';
@@ -6,8 +6,10 @@ import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
-  webClientId: '',
+  webClientId:
+    '466698973105-64alfjckctooq0bh6ekveue92a0u0vj4.apps.googleusercontent.com',
 });
+
 const LoginScreen = () => {
   const dispatch = useDispatch();
   const [initializing, setInitializing] = useState(true);
@@ -23,7 +25,16 @@ const LoginScreen = () => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
 
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
   return (
     <SafeAreaView>
       <TouchableOpacity
@@ -33,7 +44,12 @@ const LoginScreen = () => {
         <Text> Login as a Guest </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity
+        onPress={() => {
+          onGoogleButtonPress().then(res =>
+            console.log(res, 'response from google'),
+          );
+        }}>
         <Text> Login using Google </Text>
       </TouchableOpacity>
     </SafeAreaView>
