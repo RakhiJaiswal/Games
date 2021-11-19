30435d8a2648e03015166ed7f7b13b38b0e3b000
Rakhi Jaiswal <rakhijaiswal472@gmail.com>
2021-11-19 15:25:15 +0530

    Login Screen

--- src/Screens/AuthStack/Screens/LoginScreen.tsx
+++ src/Screens/AuthStack/Screens/LoginScreen.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Image,
+  StatusBar,
+} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {LoginUserAction} from '../../../store/Actions/UserAction';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {ResponsiveSize} from '../../../utils/ResponsiveSize';
+import {Colors} from '../../Games/TicTacToe/consts';
 
 GoogleSignin.configure({
   webClientId:
@@ -19,30 +29,78 @@ const LoginScreen = () => {
 
     return auth().signInWithCredential(googleCredential);
   }
-
+  const ButtonView = ({buttonPress, buttonText, icon}) => {
+    return (
+      <TouchableOpacity style={styles.button} onPress={buttonPress}>
+        <Image resizeMode="stretch" style={styles.icon} source={icon} />
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <SafeAreaView>
-      <TouchableOpacity
-        onPress={() => {
-          dispatch(LoginUserAction({userType: 'guest'}));
-        }}>
-        <Text> Login as a Guest </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => {
+    <SafeAreaView style={styles.mainContainer}>
+      <StatusBar barStyle={'light-content'} />
+      <View style={{flex: 1}} />
+      <Text style={styles.appName}> Games </Text>
+      <View style={{flex: 1}} />
+      <Text style={styles.loginText}>Login as a </Text>
+      <View style={{flex: 0.2}} />
+      <ButtonView
+        buttonText={'  Guest'}
+        icon={require('../../../assets/user.png')}
+        buttonPress={() => dispatch(LoginUserAction({userType: 'guest'}))}
+      />
+      <View style={{flex: 0.1}} />
+      <ButtonView
+        buttonText={'  Google'}
+        icon={require('../../../assets/google.png')}
+        buttonPress={() => {
           onGoogleButtonPress().then(res => {
-            let data = res.user._user;
-            console.log(res, 'response from google'),
-              dispatch(
-                LoginUserAction({userType: 'non-guest', userData: data}),
-              );
+            dispatch(
+              LoginUserAction({
+                userType: 'non-guest',
+                userData: res.user._user,
+              }),
+            );
           });
-        }}>
-        <Text> Login using Google </Text>
-      </TouchableOpacity>
+        }}
+      />
+
+      <View style={{flex: 1}} />
     </SafeAreaView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: Colors.purple,
+    alignItems: 'center',
+  },
+  appName: {
+    fontStyle: 'italic',
+    color: 'white',
+    fontSize: ResponsiveSize(70),
+    fontWeight: 'bold',
+  },
+
+  loginText: {
+    color: 'white',
+    fontSize: ResponsiveSize(30),
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: 'white',
+    paddingVertical: ResponsiveSize(15),
+    borderRadius: ResponsiveSize(25),
+    width: '50%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {height: ResponsiveSize(25), width: ResponsiveSize(25)},
+  buttonText: {fontWeight: 'bold', fontSize: ResponsiveSize(21)},
+});
