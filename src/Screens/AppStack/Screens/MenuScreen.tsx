26aecafe95370ee593675f442c7d6c360aea0061
DevInnow8 <devinnow8@gmail.com>
2021-11-22 15:24:39 +0530

    main screen done

--- src/Screens/AppStack/Screens/MenuScreen.tsx
+++ src/Screens/AppStack/Screens/MenuScreen.tsx
@@ -8,53 +8,112 @@ import {
   TouchableOpacity,
   ImageBackground,
   FlatList,
+  Image,
 } from 'react-native';
 import ProfileScreen from './ProfileScreen';
+import * as Animatable from 'react-native-animatable';
 import {TicTacToeGame, BingoGame} from './../../../assets/index';
 import {useSelector} from 'react-redux';
 const mockData = [
   {
-    name: 'Tic Tac Toe',
+    name: 'Tic Tac\n Toe',
     image: TicTacToeGame,
+    navigate: 'TicTacToe',
   },
   {
     name: 'BINGO',
     image: BingoGame,
+    navigate: 'Bingo',
   },
 ];
 const MenuScreen = ({navigation}) => {
   console.log(useSelector(state => state.UserDetailsReducer));
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
+      <View
+        style={{alignItems: 'center', justifyContent: 'center', marginTop: 20}}>
+        <Animatable.Text
+          animation="bounceIn"
+          iterationCount={'infinite'}
+          direction="alternate-reverse">
+          <Text
+            style={{
+              fontSize: 35,
+              fontWeight: '900',
+              shadowColor: 'green',
+              shadowOffset: {width: 0, height: 10},
+              shadowOpacity: 1,
+              color: 'red',
+              fontStyle: 'italic',
+            }}>
+            2 PLAYER GAMES
+          </Text>
+        </Animatable.Text>
+      </View>
+
+      <View
+        style={{
+          justifyContent: 'center',
+          backgroundColor: 'white',
+          marginTop: 20,
+        }}>
         <FlatList
           contentContainerStyle={styles.boxView}
           bounces={false}
           data={mockData}
           renderItem={({item, index}) => {
             return (
-              <View style={{}}>
-                <ImageBackground
-                  source={item.image}
-                  resizeMode="contain"
-                  style={styles.image}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      navigation.navigate('TicTacToe');
+              <View style={{marginHorizontal: 15}}>
+                <TouchableOpacity
+                  style={{
+                    // borderWidth: 5,
+                    margin: 10,
+                    height: 150,
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    shadowColor: 'rgb(57,27,151)',
+                    shadowOffset: {width: 0, height: 10},
+                    shadowOpacity: 1,
+                    shadowRadius: 1,
+                    borderRadius: 40,
+                    backgroundColor: 'rgb(57,27,151)',
+                  }}
+                  activeOpacity={1}
+                  onPress={() => {
+                    navigation.navigate(item.navigate);
+                  }}>
+                  <Text
+                    style={{
+                      marginHorizontal: 15,
+                      fontSize: 35,
+                      fontWeight: '900',
+                      shadowColor: 'green',
+                      shadowOffset: {width: 0, height: 2},
+                      shadowOpacity: 1,
+                      color: 'red',
+                      shadowRadius: 1,
+                      borderRadius: 40,
+                      fontStyle: 'italic',
                     }}>
-                    <Text> {item.name}</Text>
-                  </TouchableOpacity>
-                </ImageBackground>
+                    {item.name}
+                  </Text>
+                  <Image
+                    source={item.image}
+                    style={{
+                      height: 80,
+                      width: 150,
+                      transform: [{rotate: '40deg'}],
+                      marginHorizontal: 15,
+                    }}
+                    resizeMode="contain"
+                  />
+                </TouchableOpacity>
               </View>
             );
           }}
         />
       </View>
-
-      <TouchableOpacity>
-        <Text> Bingo </Text>
-      </TouchableOpacity>
       <ProfileScreen />
     </SafeAreaView>
   );
@@ -70,4 +129,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  boxView: {},
 });
--- src/Screens/AppStack/Screens/ProfileScreen.tsx
+++ src/Screens/AppStack/Screens/ProfileScreen.tsx
@@ -7,15 +7,41 @@ const ProfileScreen = () => {
   const dispatch = useDispatch();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{marginHorizontal: 50, marginTop: 20}}>
       {/* <Text>ProfileScreen </Text>
       <Text> Name </Text>
       <Text> Profile Image</Text> */}
       <TouchableOpacity
+        style={{
+          margin: 5,
+          height: 50,
+          alignItems: 'center',
+          shadowColor: 'yellow',
+          shadowOffset: {width: 0, height: 2},
+          shadowOpacity: 1,
+          shadowRadius: 1,
+          borderRadius: 40,
+          backgroundColor: 'yellow',
+          justifyContent: 'center',
+        }}
         onPress={() => {
           dispatch(LogoutUserAction());
         }}>
-        <Text> Logout </Text>
+        <Text
+          style={{
+            marginHorizontal: 15,
+            fontSize: 25,
+            fontWeight: '900',
+            shadowColor: 'green',
+            shadowOffset: {width: 0, height: 2},
+            shadowOpacity: 1,
+            color: 'red',
+            shadowRadius: 1,
+            borderRadius: 40,
+            fontStyle: 'italic',
+          }}>
+          Logout{' '}
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
